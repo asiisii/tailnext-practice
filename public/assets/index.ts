60ed2624f86567c136c2acb4b39c4f7325a61af5
Ashish Malla <asiisii@asiisiis-mac.local>
2023-06-01 11:30:11 -0600

    add anisha,ali,and richard avatar for testimonials

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import illustrationIntro from './illustration-intro.svg';
+import logo from './logo.svg'
+import illustrationIntro from './illustration-intro.svg'
+import anishaAvatar from './avatar-anisha.png'
+import aliAvatar from './avatar-ali.png'
+import richardAvatar from './avatar-richard.png'
 
 export {
     logo,
-    illustrationIntro
+    illustrationIntro,
+    anishaAvatar,
+    aliAvatar,
+    richardAvatar,
 }
