1d1c3d56c00f15dbe1561d54546e84e6823412a7
Ashish Malla <asiisii@asiisiis-mac.local>
2023-06-01 11:30:42 -0600

    render navbar and testimonialsection components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { FeatureSection } from './components/FeatureSection';
 import { HeroSection } from './components/HeroSection';
+import Navbar from './components/Navbar';
+import { TestimonialsSection } from './components/TestimonialsSection';
 
 export default function Page() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <FeatureSection />
+      <TestimonialsSection />
     </>
   );
 }
