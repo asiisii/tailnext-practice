0597b1d6fda7c1c747ce824497616661ae261c9e
Ashish Malla <asiisii@asiisiis-mac.local>
2023-06-01 12:31:27 -0600

    add icons for footer

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -3,6 +3,12 @@ import illustrationIntro from './illustration-intro.svg'
 import anishaAvatar from './avatar-anisha.png'
 import aliAvatar from './avatar-ali.png'
 import richardAvatar from './avatar-richard.png'
+import logoWhite from './logo-white.svg'
+import facebookIcon from './icon-facebook.svg'
+import youtubeIcon from './icon-youtube.svg'
+import twitterIcon from './icon-twitter.svg'
+import pinterestIcon from './icon-pinterest.svg'
+import instagramIcon from './icon-instagram.svg'
 
 export {
     logo,
@@ -10,4 +16,10 @@ export {
     anishaAvatar,
     aliAvatar,
     richardAvatar,
+    logoWhite,
+    facebookIcon,
+    youtubeIcon,
+    twitterIcon,
+    pinterestIcon,
+    instagramIcon,
 }
