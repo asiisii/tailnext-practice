251405fb0eb09ed59d79c52b02afa3e6c41a61a5
asiisii <ashishmalla45@gmail.com>
2023-06-22 20:04:45 -0600

    fix deployment error

--- app/components/TestimonialsSection.tsx
+++ app/components/TestimonialsSection.tsx
@@ -10,7 +10,7 @@ export const TestimonialsSection = () => (
   >
     {/* heading */}
     <h2 className='text-4xl font-bold text-center'>
-      What's Different About Manage?
+      What&apos;s Different About Manage?
     </h2>
     {/* Testimonials Container */}
     <div className='flex flex-col mt-24 md:flex-row md:space-x-6'>
@@ -19,9 +19,9 @@ export const TestimonialsSection = () => (
         <Image src={anishaAvatar} alt='avatar nisha' className='w-16 -mt-14' />
         <h5 className='text-lg font-bold'>Anisha Li</h5>
         <p className='text-sm text-darkGrayishBlue'>
-          "Manage has supercharged out team's workflow. The ability to maintain
-          visibility on larger milesstones at all times keeps everyone
-          motivated."
+          &quot;Manage has supercharged out team&apos;s workflow. The ability to
+          maintain visibility on larger milesstones at all times keeps everyone
+          motivated.&quot;
         </p>
       </div>
       {/* testimonial 2 */}
@@ -29,9 +29,9 @@ export const TestimonialsSection = () => (
         <Image src={aliAvatar} alt='avatar ali' className='w-16 -mt-14' />
         <h5 className='text-lg font-bold'>Ali Bravo</h5>
         <p className='text-sm text-darkGrayishBlue'>
-          "We have been able to cancel so many subscriptions since using Manage.
+          &quot;We have been able to cancel so many subscriptions since using Manage.
           There is no more cross-channel confusion and everyone is much more
-          focused."
+          focused.&quot;
         </p>
       </div>
       {/* testimonial 3 */}
@@ -39,8 +39,9 @@ export const TestimonialsSection = () => (
         <Image src={richardAvatar} alt='avatar nisha' className='w-16 -mt-14' />
         <h5 className='text-lg font-bold'>Richard Watts</h5>
         <p className='text-sm text-darkGrayishBlue'>
-          "Manage allows us to provide structure and product keeps us organized
-          and focused. I can't stop recommending them to everyone I talk to."
+          &quot;Manage allows us to provide structure and product keeps us organized
+          and focused. I can&apos;t stop recommending them to everyone I talk
+          to.&quot;
         </p>
       </div>
     </div>
